027643f41fdc4c909c17345a3b7f6305cf76976e
Erik Verna <vernaerik@gmail.com>
2024-11-28 21:50:03 -0300

    arreglada clase 8

--- Clase 8/ejercicio1.py
+++ Clase 8/ejercicio1.py
@@ -5,7 +5,22 @@ Escribi un programa que permita:
 - Seleccionar un producto a partir de su codigo
 - Ingresar la cantidad vendida (que debe ser mayor que cero).
 - Actualizar la cantidad en stock de ese producto restando la cantidad vendida.
+
+El arreglo de productos disponibles es el que ves a continuación:
+["P001", "Manzanas", 50],
+["P002", "Peras", 40],
+["P001", "Bananas", 30],
+["P001", "Naranjas", 60]
+
+El script que tenes que hacer debe modificar la cantidad en stock de acuerdo a cada venta realizada. Si la cantidad
+vendida es mayor que la cantidad disponible en stock, el programa debe mostrar un mensaje de error.
 """
 
-inventario = []
+inventario = [
+["P001", "Manzanas", 50],
+["P002", "Peras", 40],
+["P001", "Bananas", 30],
+["P001", "Naranjas", 60]
+]
+
 

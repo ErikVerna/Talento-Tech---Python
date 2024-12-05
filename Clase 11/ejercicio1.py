45a9120a8862f9ecb59da9ab7a24bc70e08c1c1e
Erik Verna <vernaerik@gmail.com>
2024-12-05 13:57:40 -0300

    agregado ejercicios clase 11

--- Clase 11/ejercicio1.py
+++ Clase 11/ejercicio1.py
@@ -1,14 +1,26 @@
+"""
+Imaginá que en tu tienda querés implementar un sistema de descuentos automaticos. Vas a desarrollar un
+programa que permita calcular el precio final de un producto despues de aplicar el descuento.
+
+Para hacerlo:
+- Crea una función que reciba como parámetros el precio original del producto y el porcentaje de descuento,
+y que retorne el precio final con el descuento aplicado.
+
+Luego, pedi que se ingrese el precio y el porcentaje de descuento.
+Mostra el precio final despues de aplicar el descuento.
+"""
+
+
+
 def calcular_valor_final(precio, descuento):
     return precio - (precio * descuento / 100)
 
-precio = float(input("Ingrese el precio: $"))
-descuento = float(input("Ingrese el porcentaje de descuento: " + "%"))
-
 try: 
-    calcular_valor_final(precio, descuento)
-except:
-    
+    precio = float(input("Ingrese el precio: $"))
+    descuento = float(input("Ingrese el porcentaje de descuento: %"))
+    precio_final = calcular_valor_final(precio, descuento)
+except ValueError as e:
+    print(f"ERROR {e} \n ### Por favor, ingrese un valor numerico ###")
 
-precio_final = calcular_valor_final(precio, descuento)
 
-print(f"El precio final con el descuento es de : ${precio_final}")
+print(f"El precio final con el descuento del {descuento}% es de: ${precio_final}")
--- Clase 11/ejercicio2.py
+++ Clase 11/ejercicio2.py
@@ -1,8 +1,40 @@
 """
+Desarrollá un programa que permita calcular el promedio de ventas
+de la tienda. Para esto:
 
+- Creá una función que reciba como párametro una lista de ventas diarias y
+devuelva el promedio de esas ventas.
+
+- Con una función, solicitá a la persona que ingrese las ventas de cada dia durante
+la cantidad de dias que elija el usuario.
+
+Usá las funciones para calcular y mostrar el prmedio de ventas al finalizar.
 """
 
-def calculo_promedio(ventas):
+def calculo_promedio(ventas = []):
     promedio = 0
     ventas_totales = 0
-    
+    for venta in ventas:
+        ventas_totales = ventas_totales + venta
+    cantidad_ventas = len(ventas)
+    promedio = ventas_totales / cantidad_ventas
+    return promedio
+
+def usuario_ventas(dias):
+    ventas = []
+    for dia in range(dias):
+        ventas_dia = float(input("Ingrese las ventas del dia: "))
+        ventas.append(ventas_dia)
+    return ventas
+
+while True:
+    print("Si desea salir de la aplicación, ingrese como valor 00")
+    dias = int(input("¿Cuantos dias desea calcular?: "))
+    if dias == 00:
+        print("FIN DE PROGRAMA \n ¡Gracias por utilizar la aplicación!")
+        break
+    else:
+        ventas_totales = usuario_ventas(dias)
+        promedio = calculo_promedio(ventas_totales)
+        print(f"El promedio de {dias} dias es: {promedio:.2f}")
+
